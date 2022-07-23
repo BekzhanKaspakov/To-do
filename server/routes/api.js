6f6204a753e59826f341d595d800c2911a54cad3
Bekzhan Kaspakov <bekzhan.kaspakov@gmail.com>
2022-07-23 16:54:30 +0600

    added mongo and first route to get tasks

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,9 +1,27 @@
-var express = require('express');
-var router = express.Router();
+const Router = require("express");
+const dbo = require("../db/conn");
+
+var router = Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("Hello World");
+});
+
+router.get("/tasks", function (req, res, next) {
+  const dbConnect = dbo.getDb();
+
+  dbConnect
+    .collection("tasks")
+    .find({})
+    .limit(50)
+    .toArray(function (err, result) {
+      if (err) {
+        res.status(400).send("Error fetching listings!");
+      } else {
+        res.json(result);
+      }
+    });
 });
 
 module.exports = router;

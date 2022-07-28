585d3cbba05edb91cbe5654a47c067b2aadebb33
Bekzhan Kaspakov <bekzhan.kaspakov@gmail.com>
2022-07-28 18:22:18 +0600

    added login endpoint

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,6 +3,7 @@ const Joi = require("joi");
 const bcrypt = require("bcrypt");
 const User = require("../db/userModel");
 const Task = require("../db/taskModel");
+const jwt = require("jsonwebtoken");
 
 const schema = Joi.object({
   username: Joi.string().min(3).max(30).required(),
@@ -133,6 +134,59 @@ router.post("/register", function (req, res) {
     .catch((e) => {
       res.status(500).send({
         message: "Password was not hashed successfully",
+        e,
+      });
+    });
+});
+
+router.post("/login", function (req, res) {
+  // check if email exists
+  User.findOne({ email: req.body.email })
+
+    // if email exists
+    .then((user) => {
+      // compare the password entered and the hashed password found
+      bcrypt
+        .compare(req.body.password, user.password)
+
+        // if the passwords match
+        .then((passwordCheck) => {
+          // check if password matches
+          if (!passwordCheck) {
+            return res.status(400).send({
+              message: "Passwords does not match",
+            });
+          }
+
+          //   create JWT token
+          const token = jwt.sign(
+            {
+              userId: user._id,
+              userEmail: user.email,
+            },
+            "RANDOM-TOKEN",
+            { expiresIn: "24h" }
+          );
+
+          //   return success response
+          res.status(200).send({
+            message: "Login Successful",
+            email: user.email,
+            token,
+          });
+        })
+        // catch error if password does not match
+        .catch((error) => {
+          res.status(400).send({
+            message: "Passwords does not match",
+            error,
+          });
+        });
+    })
+    // catch error if email does not exist
+    .catch((e) => {
+      res.status(404).send({
+        message: "Email not found",
         e,
       });
     });

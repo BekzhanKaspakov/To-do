f80d1cdb98837e922f46f7b1f618dcca07246172
Bekzhan Kaspakov <bekzhan.kaspakov@gmail.com>
2022-08-09 02:38:45 +0600

    edited cors to only allow client domain and fixed warnings for netlify deploy

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,5 +1,5 @@
 import { compose, createStore, applyMiddleware } from "redux";
-import { persistStore, persistReducer } from "redux-persist";
+import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import logger from "redux-logger";
 import thunk from "redux-thunk";
